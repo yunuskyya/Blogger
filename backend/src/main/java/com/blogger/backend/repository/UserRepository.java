b7c9594ebd89a8009620bc09206d379039f133ff
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 01:55:54 +0300

    User Repository oluşturuldu.

---
+++ backend/src/main/java/com/blogger/backend/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.blogger.backend.repository;
+
+
+import java.util.Optional;
+
+import com.blogger.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+}
+
