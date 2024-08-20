352bd79cb67e42ee74c6ba8c690d47838894d759
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 10:43:11 +0300

    NotUniqueEmailException  oluşturuldu.

---
+++ backend/src/main/java/com/blogger/backend/exception/NotUniqueEmailException.java
@@ -0,0 +1,20 @@
+package com.blogger.backend.exception;
+
+import java.util.Collections;
+import java.util.Map;
+
+import com.blogger.backend.shared.Messages;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+
+public class NotUniqueEmailException extends RuntimeException {
+    public NotUniqueEmailException() {
+        super(Messages.getMessageForLocale("blogger.validation.notunique.email", LocaleContextHolder.getLocale()));
+    }
+
+    public Map<String, String> getValidationErrors() {
+        return Collections.singletonMap("email", Messages.getMessageForLocale("logim.validation.notunique.email",
+                LocaleContextHolder.getLocale()));
+    }
+}
+
