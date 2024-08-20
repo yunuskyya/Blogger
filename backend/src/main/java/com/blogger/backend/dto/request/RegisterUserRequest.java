b0a1dbbc8774b7a1a29cd17504b8780f12152708
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 19:34:16 +0300

    RegisterUserRequest: güncellendi.

--- backend/src/main/java/com/blogger/backend/dto/request/RegisterUserRequest.java
+++ backend/src/main/java/com/blogger/backend/dto/request/RegisterUserRequest.java
@@ -12,17 +12,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterUserRequest{
-    @NotBlank(message = "blogger.validation.notBlank.username")
-    @Size(min = 3, max = 150, message ="blogger.validation.size.username")
+    @NotBlank(message = "{blogger.validation.notBlank.firstName}")
+    @Size(min = 3, max = 150, message ="{blogger.validation.size.firstName}")
+    private String firstName;
+    @NotBlank(message = "{blogger.validation.notBlank.lastName}")
+    @Size(min = 3, max = 150, message ="{blogger.validation.size.lastName}")
+    private String lastName;
+    @NotBlank(message = "{blogger.validation.notBlank.phoneNumber}")
+    @Size(min = 11, max = 13, message ="{blogger.validation.size.phoneNumber}")
+    private String phoneNumber;
+    @NotBlank(message = "{blogger.validation.notBlank.username}")
+    @Size(min = 3, max = 150, message ="{blogger.validation.size.username}")
     private String username;
 
-    @NotBlank(message = "blogger.validation.notBlank.email")
-    @Size(min = 3, max = 150, message = "blogger.validation.size.email")
+    @NotBlank(message = "{blogger.validation.notBlank.email}")
+    @Size(min = 3, max = 150, message = "{blogger.validation.size.email}")
     @UniqueEmail
     private String email;
 
-    @Size(min = 8, max = 50, message = "blogger.validation.size.password")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\.]).{8,}$", message = "blogger.validation.pattern.password")
+    @Size(min = 8, max = 50, message = "{blogger.validation.size.password}")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\.]).{8,}$", message = "{blogger.validation.pattern.password}")
     private String password;
 
 }
