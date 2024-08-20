c892cd974845eb9f9330cdae8690230086578603
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 10:48:16 +0300

    CredentialsRequest DTO sınıfı oluşturuldu.

---
+++ backend/src/main/java/com/blogger/backend/dto/request/CredentialsRequest.java
@@ -0,0 +1,7 @@
+package com.blogger.backend.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record CredentialsRequest(@Email String email, @NotBlank String password, String username) {
+}
