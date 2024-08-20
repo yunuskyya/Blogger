910fd96a07529748db173e51d4dc09b93ab3c64d
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 10:54:59 +0300

    AuthService oluşturuldu.

---
+++ backend/src/main/java/com/blogger/backend/service/AuthService.java
@@ -0,0 +1,13 @@
+package com.blogger.backend.service;
+
+import com.blogger.backend.dto.request.CredentialsRequest;
+import com.blogger.backend.dto.response.AuthResponse;
+
+public interface AuthService {
+
+    AuthResponse authenticate(CredentialsRequest credentials);
+
+    void logout(String authorizationHeader);
+}
+
+
