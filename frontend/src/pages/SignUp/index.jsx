ee6631da7db9b8f204a59696611c91a53fd9f876
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 21:40:08 +0300

    password, passwordRepeat ile eşleşmediği zaman hata mesajı döndürme işlemi yapıldı.

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import { signUp } from "./api";
+import { Input } from "./components/Input";
 
 export function SignUp() {
   const [firstName, setFirstName] = useState("");
@@ -18,9 +19,56 @@ export function SignUp() {
   const [passwordMatchError, setPasswordMatchError] = useState(false);
 
   useEffect(() => {
-    setErrors({});
-    setPasswordMatchError(false);
-  }, [firstName, lastName, phoneNumber, username, email, password, passwordRepeat]);
+    setErrors(function(lasterErrors) {
+      return {
+        ...lasterErrors,
+        firstName: undefined,
+      };
+    });
+  }, [firstName]);
+  useEffect(() => {
+    setErrors(function(lasterErrors) {
+      return {
+        ...lasterErrors,
+        firstName: undefined,
+      };
+    });
+  }, [firstName]);
+  useEffect(() => {
+    setErrors(function(lasterErrors) {
+      return {
+        ...lasterErrors,
+        lastName: undefined,
+      };
+    });
+  }, [lastName]);
+  useEffect(() => {
+    setErrors(function(lasterErrors) {
+      return {
+        ...lasterErrors,
+        phoneNumber: undefined,
+      };
+    });
+  }, [phoneNumber]);
+  useEffect(() => {
+    setErrors(function(lasterErrors) {
+      return {
+        ...lasterErrors,
+        username: undefined,
+      };
+    });
+  }, [username]);
+  useEffect(() => {
+    setErrors(function(lasterErrors) {
+      return {
+        ...lasterErrors,
+        password: undefined,
+      };
+    });
+  }, [password]);
+
+
+
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -54,7 +102,13 @@ export function SignUp() {
       setApiProgress(false);
     }
   };
-
+  const passwordRepeatError = useMemo(() => {
+    if( password && password!== passwordRepeat){
+      return 'Passwords do not match';
+    }
+    return '';
+  },[password, passwordRepeat]);
+  
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
       <div
@@ -69,34 +123,20 @@ export function SignUp() {
         </div>
         <div className="card-body">
           <form onSubmit={onSubmit}>
-            <div className="mb-3">
-              <label htmlFor="firstName" className="form-label">
-                First Name
-              </label>
-              <input
-                id="firstName"
-                className={
-                  errors.firstName ? "form-control is-invalid" : "form-control"
-                }
-                onChange={(event) => setFirstName(event.target.value)}
-                required
-              />
-              <div className="invalid-feedback">{errors.firstName}</div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="lastName" className="form-label">
-                Last Name
-              </label>
-              <input
-                id="lastName"
-                className={
-                  errors.lastName ? "form-control is-invalid" : "form-control"
-                }
-                onChange={(event) => setLastName(event.target.value)}
-                required
-              />
-              <div className="invalid-feedback">{errors.lastName}</div>
-            </div>
+            <Input
+              id="firstName"
+              label="First Name"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+              error={errors.firstName}
+            />
+            <Input
+              id="lastName"
+              label="Last Name"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+              error={errors.lastName}
+            />
             <div className="mb-3">
               <label htmlFor="phoneNumber" className="form-label">
                 Phone Number
@@ -117,70 +157,40 @@ export function SignUp() {
               />
               <div className="invalid-feedback">{errors.phoneNumber}</div>
             </div>
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">
-                Username
-              </label>
-              <input
-                id="username"
-                className={
-                  errors.username ? "form-control is-invalid" : "form-control"
-                }
-                onChange={(event) => setUsername(event.target.value)}
-                required
-              />
-              <div className="invalid-feedback">{errors.username}</div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                id="email"
-                className={
-                  errors.email ? "form-control is-invalid" : "form-control"
-                }
-                type="email"
-                onChange={(event) => setEmail(event.target.value)}
-                required
-              />
-              <div className="invalid-feedback">{errors.email}</div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <input
-                id="password"
-                className={
-                  errors.password ? "form-control is-invalid" : "form-control"
-                }
-                type="password"
-                onChange={(event) => setPassword(event.target.value)}
-                required
-              />
-              <div className="invalid-feedback">{errors.password}</div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="passwordRepeat" className="form-label">
-                Repeat Password
-              </label>
-              <input
-                id="passwordRepeat"
-                className={
-                  passwordMatchError ? "form-control is-invalid" : "form-control"
-                }
-                type="password"
-                onChange={(event) => setPasswordRepeat(event.target.value)}
-                required
-              />
-              <div className="invalid-feedback">
-                {passwordMatchError && "Passwords do not match"}
-              </div>
-            </div>
+            <Input
+              id="username"
+              label="Username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              error={errors.username}
+            />
+            <Input
+              id="email"
+              label="Email"
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              error={errors.email}
+            />
+            <Input
+              id="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              error={errors.password}
+            />
+            <Input
+              id="passwordRepeat"
+              label="Repeat Password"
+              type="password"
+              value={passwordRepeat}
+              onChange={(event) => setPasswordRepeat(event.target.value)}
+              error={passwordRepeatError}
+            />
             <div>
               {successMessage && (
-                <div className="alert alert-success"> {successMessage}</div>
+                <div className="alert alert-success">{successMessage}</div>
               )}
             </div>
             <button
