963586488c362e22e154b3d6731af7770e1dde54
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 10:56:52 +0300

    SecurityBeans configuration sınıfı oluşturuldu.

---
+++ backend/src/main/java/com/blogger/backend/config/SecurityBeans.java
@@ -0,0 +1,15 @@
+package com.blogger.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityBeans {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
