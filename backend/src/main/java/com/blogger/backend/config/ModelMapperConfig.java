d04f0c9f02b18394ed0692364726c9a1fd4d75b7
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 10:55:30 +0300

    ModelMapperConfig güncellendi.

--- backend/src/main/java/com/blogger/backend/config/ModelMapperConfig.java
+++ backend/src/main/java/com/blogger/backend/config/ModelMapperConfig.java
@@ -1,4 +1,33 @@
 package com.blogger.backend.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Bu sınıf, ModelMapper'ı yapılandırmak için kullanılır
+// ModelMapper, nesneler arasında veri aktarımını kolaylaştıran bir kütüphanedir
+// DTO'lar ve entity'ler arasında veri aktarımı yaparken kullanılır
+@Configuration
 public class ModelMapperConfig {
+
+    @Bean
+    @Qualifier("modelMapperForResponse")
+    public ModelMapper modelMapperForResponse() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true)
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
+    }
+
+    @Bean
+    @Qualifier("modelMapperForRequest")
+    public ModelMapper modelMapperForRequest() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+    }
 }
+
