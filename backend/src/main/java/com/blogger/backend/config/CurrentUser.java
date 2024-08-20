0b8db5cc0775497a6b3bf1760be68c2177eba861
yunuskyya <0yunuskaya0@gmail.com>
2024-08-20 10:52:33 +0300

    CurrentUser oluşturuldu.

--- backend/src/main/java/com/blogger/backend/config/CurrentUser.java
+++ backend/src/main/java/com/blogger/backend/config/CurrentUser.java
@@ -1,4 +1,73 @@
 package com.blogger.backend.config;
 
-public class CurrentUser {
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.blogger.backend.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Data;
+
+/*
+ * Bu sınıf, güvenlik işlemleri sırasında oturum açmış olan userın  bilgilerini tutar
+ * UserDetails interface'i, kullanıcı bilgilerini tutan metotları tanımlar
+ */
+
+@Data
+public class CurrentUser implements UserDetails {
+    private int id;
+    private String username;
+    private String password;
+    private boolean enabled;
+    private Collection<? extends GrantedAuthority> authorities;
+
+
+    public CurrentUser(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.enabled = user.isActive();
+        this.authorities = user.getAuthorities().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
 }
+
