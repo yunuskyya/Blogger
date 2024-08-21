bdd65dad8ae38a08fde1da5a31ad304e34bb46c1
yunuskyya <0yunuskaya0@gmail.com>
2024-08-21 14:06:35 +0300

    LocaleConfig: sınıfı oluşturuldu

---
+++ backend/src/main/java/com/blogger/backend/config/LocaleConfig.java
@@ -0,0 +1,29 @@
+package com.blogger.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver slr = new AcceptHeaderLocaleResolver();
+        slr.setDefaultLocale(Locale.forLanguageTag("en")); // Varsayılan dil Türkçe
+        return slr;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        registry.addInterceptor(lci);
+    }
+}
