3bc41043fb8b79217be4eea3197b53a95d42c183
yunuskyya <0yunuskaya0@gmail.com>
2024-08-21 13:59:40 +0300

    dil şeceneği ekelndi

--- backend/src/main/java/com/blogger/backend/config/SpringDocConfig.java
+++ backend/src/main/java/com/blogger/backend/config/SpringDocConfig.java
@@ -1,19 +1,33 @@
 package com.blogger.backend.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
 
 @OpenAPIDefinition
 @Configuration
 public class SpringDocConfig {
+
     @Bean
     public OpenAPI baseOpenAPI() {
-
         return new OpenAPI().info(new Info().title("Spring Doc").version("1.0.0").description("Spring doc"));
     }
-}
 
+    @Bean
+    public OpenApiCustomizer customizeGlobalHeaders() {
+        return openApi -> openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations()
+                .forEach(operation -> operation.addParametersItem(new Parameter()
+                        .in("header")
+                        .schema(new StringSchema())
+                        .name("Accept-Language")
+                        .description("Language preference")
+                        .required(false)
+                        .example("en"))));
+    }
+}
