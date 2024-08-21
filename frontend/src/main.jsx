e0142bbc5e98afac58851654a428d341f881a6c4
yunuskyya <0yunuskaya0@gmail.com>
2024-08-21 14:01:29 +0300

    güncellendi.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { SignUp } from './pages/SignUp'
-//import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { SignUp } from "./pages/SignUp/index.jsx";
+import "./locales";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <SignUp />  
-  </StrictMode>,
-)
+    <SignUp />
+  </StrictMode>
+);
