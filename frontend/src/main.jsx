b46a7984fd89b906dcf67c7678f1b7ae3a65741c
yunuskyya <0yunuskaya0@gmail.com>
2024-08-22 23:40:32 +0300

    main.jsx güncellendi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { SignUp } from "./pages/SignUp/index.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./locales";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <SignUp />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
