69b49164030dfdc35e348add425f33628d340edb
yunuskyya <0yunuskaya0@gmail.com>
2024-08-22 17:14:14 +0300

    UserServiceImpl kullanıcı kayıt işleminde kullanıcıya mail gönderme işlemi yapıldı.

--- backend/src/main/java/com/blogger/backend/service/Impl/UserServiceImpl.java
+++ backend/src/main/java/com/blogger/backend/service/Impl/UserServiceImpl.java
@@ -1,32 +1,36 @@
 package com.blogger.backend.service.Impl;
 
-import com.blogger.backend.config.ModelMapperConfig;
 import com.blogger.backend.dto.request.RegisterUserRequest;
 import com.blogger.backend.model.User;
 import com.blogger.backend.model.enums.Role;
 import com.blogger.backend.repository.UserRepository;
+import com.blogger.backend.service.MailService;
 import com.blogger.backend.service.UserService;
-import com.blogger.backend.token.TokenService;
 import jakarta.transaction.Transactional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mail.MailException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.UUID;
+
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final MailService mailService;
     private final UserRepository userRepository;
     private final ModelMapper modelMapperForResponse;
     private final ModelMapper modelMapperForRequest;
     private final PasswordEncoder passwordEncoder;
     private static final Logger logger = LogManager.getLogger(UserServiceImpl.class);
 
-    public UserServiceImpl(UserRepository userRepository, @Qualifier("modelMapperForResponse") ModelMapper modelMapperForResponse,
+    public UserServiceImpl(MailService mailService, UserRepository userRepository, @Qualifier("modelMapperForResponse") ModelMapper modelMapperForResponse,
                            @Qualifier("modelMapperForRequest") ModelMapper modelMapperForRequest, PasswordEncoder passwordEncoder) {
+        this.mailService = mailService;
         this.userRepository = userRepository;
         this.modelMapperForResponse = modelMapperForResponse;
         this.modelMapperForRequest = modelMapperForRequest;
@@ -35,16 +39,16 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
-    @Transactional
+    @Transactional(rollbackOn = MailException.class)
     public void registerUser(RegisterUserRequest registerUserRequest) {
-        User newUser = modelMapperForRequest.map(registerUserRequest, User.class);
-        newUser.setPassword(passwordEncoder.encode(registerUserRequest.getPassword()));
-        newUser.setAuthorities(new HashSet<>() {{
-            add(Role.ROLE_USER);
-        }});
-        newUser.setActive(true);
-        userRepository.save(newUser);
-        logger.info("User registered: {}", newUser.getUsername());
+            User newUser = modelMapperForRequest.map(registerUserRequest, User.class);
+            newUser.setPassword(passwordEncoder.encode(registerUserRequest.getPassword()));
+            newUser.setActivationToken(UUID.randomUUID().toString());
+            newUser.setAuthorities(new HashSet<>() {{
+                add(Role.ROLE_USER);
+            }});
+            userRepository.saveAndFlush(newUser);
+            mailService.sendAccountActivationEmail(newUser.getEmail(),newUser.getActivationToken());
+            logger.info("User registered: {}", newUser.getUsername());
     }
-
 }
