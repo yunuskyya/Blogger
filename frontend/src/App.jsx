8ffd6a48b452faa14c2462d3d42bbe2a579d694d
yunuskyya <0yunuskaya0@gmail.com>
2024-08-23 01:11:56 +0300

    App.jsx navbar eklendi.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link, Outlet } from "react-router-dom";
+import logo from "./assets/ad.png";
+import { LanguageSelector } from "./shared/components/LanguageSelector";
+import { useTranslation } from "react-i18next";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { t } = useTranslation();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img
+              src={logo}
+              alt="Blog Logo"
+              style={{ width: "50px", height: "auto" }}
+            />
+            Blogger
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/signup">
+                  {t('signUp')}
+                </Link>
+              </li>
+              <li className="nav-item d-flex align-items-center">
+                <LanguageSelector />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container mt-3">
+        <Outlet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
