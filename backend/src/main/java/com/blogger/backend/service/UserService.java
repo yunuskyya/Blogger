5ab17648f4018a99a3cde7d02b688902e8b26fac
yunuskyya <0yunuskaya0@gmail.com>
2024-08-22 23:38:36 +0300

    activationUser metodu oluşturuldu.

--- backend/src/main/java/com/blogger/backend/service/UserService.java
+++ backend/src/main/java/com/blogger/backend/service/UserService.java
@@ -4,6 +4,7 @@ import com.blogger.backend.dto.request.RegisterUserRequest;
 
 public interface UserService {
      void registerUser(RegisterUserRequest request);
+     void activationUser(String token);
 
 
 }
