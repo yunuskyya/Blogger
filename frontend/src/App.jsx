94af5649818b1818e16e26304db662a8a790776e
yunuskyya <0yunuskaya0@gmail.com>
2024-08-23 13:30:24 +0300

    navbar alanı çıkarıldı

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,10 @@
-import { Link, Outlet } from "react-router-dom";
-import logo from "./assets/ad.png";
-import { LanguageSelector } from "./shared/components/LanguageSelector";
-import { useTranslation } from "react-i18next";
+import { Outlet } from "react-router-dom";
+import { NavBar } from "./shared/components/Navbar";
 
 function App() {
-  const { t } = useTranslation();
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            <img
-              src={logo}
-              alt="Blog Logo"
-              style={{ width: "50px", height: "auto" }}
-            />
-            Blogger
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/signup">
-                  {t('signUp')}
-                </Link>
-              </li>
-              <li className="nav-item d-flex align-items-center">
-                <LanguageSelector />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <NavBar />
       <div className="container mt-3">
         <Outlet />
       </div>
