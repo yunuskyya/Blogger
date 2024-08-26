3492d1f3635fd04cb948a35cefda69318a3345a1
yunuskyya <0yunuskaya0@gmail.com>
2024-08-26 15:55:29 +0300

    CredentialsRequest güncellendi.

--- backend/src/main/java/com/blogger/backend/dto/request/CredentialsRequest.java
+++ backend/src/main/java/com/blogger/backend/dto/request/CredentialsRequest.java
@@ -3,5 +3,5 @@ package com.blogger.backend.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record CredentialsRequest(@Email String email, @NotBlank String password, String username) {
+public record CredentialsRequest(@Email String email, @NotBlank String password, String username,String phoneNumber) {
 }
