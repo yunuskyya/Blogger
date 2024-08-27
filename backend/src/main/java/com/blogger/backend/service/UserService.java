620d864f4ce4cf92ddcc534e1f95cc3070369e2e
yunuskyya <0yunuskaya0@gmail.com>
2024-08-28 00:52:17 +0300

    UserService getAllUsers metodu oluşturuldu.

--- backend/src/main/java/com/blogger/backend/service/UserService.java
+++ backend/src/main/java/com/blogger/backend/service/UserService.java
@@ -1,10 +1,14 @@
 package com.blogger.backend.service;
 
 import com.blogger.backend.dto.request.RegisterUserRequest;
+import com.blogger.backend.dto.response.GetAllUserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UserService {
      void registerUser(RegisterUserRequest request);
      void activationUser(String token);
+     Page<GetAllUserResponse> getAllUsers(Pageable pageable);
 
 
 }
