37a97338fd37c2a9be2b840b4671e64dfa9e6522
yunuskyya <0yunuskaya0@gmail.com>
2024-08-28 00:51:27 +0300

    GetAllUserResponse güncellendi.

--- backend/src/main/java/com/blogger/backend/dto/response/GetAllUserResponse.java
+++ backend/src/main/java/com/blogger/backend/dto/response/GetAllUserResponse.java
@@ -5,6 +5,11 @@ import lombok.Data;
 @Data
 public class GetAllUserResponse {
 
+    private int id;
     private String username;
+    private String email;
+    private String phoneNumber;
+    private boolean active;
+    private boolean isDeleted;
 }
 
