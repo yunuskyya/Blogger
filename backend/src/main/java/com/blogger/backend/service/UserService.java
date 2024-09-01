7500172deb8b558bdc6144554a6698c80b1a02c0
yunuskyya <0yunuskaya0@gmail.com>
2024-09-01 10:33:48 +0300

    UserService getUserById metotudu eklendi.

--- backend/src/main/java/com/blogger/backend/service/UserService.java
+++ backend/src/main/java/com/blogger/backend/service/UserService.java
@@ -2,6 +2,7 @@ package com.blogger.backend.service;
 
 import com.blogger.backend.dto.request.RegisterUserRequest;
 import com.blogger.backend.dto.response.GetAllUserResponse;
+import com.blogger.backend.dto.response.GetUserByIdResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,6 +10,7 @@ public interface UserService {
      void registerUser(RegisterUserRequest request);
      void activationUser(String token);
      Page<GetAllUserResponse> getAllUsers(Pageable pageable);
+     GetUserByIdResponse getUserById(int id);
 
 
 }
