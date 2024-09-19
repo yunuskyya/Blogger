48da98f49eec8ed9fcb80f1d62d8836d4001d6d4
yunuskyya <0yunuskaya0@gmail.com>
2024-09-19 09:56:00 +0300

    Kod iyleştirmesi yapıldı.

--- backend/src/main/java/com/blogger/backend/repository/UserRepository.java
+++ backend/src/main/java/com/blogger/backend/repository/UserRepository.java
@@ -9,9 +9,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
+
     Optional<User> findByPhoneNumber(String phoneNumber);
+
     User findByActivationToken(String token);
 
 }
