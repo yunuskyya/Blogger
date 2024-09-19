6028052a7e6e38fb0e6dea92ae8f6bf6dcd78fee
yunuskyya <0yunuskaya0@gmail.com>
2024-09-19 10:00:03 +0300

    Mail service interface güncellendi.

--- backend/src/main/java/com/blogger/backend/service/MailService.java
+++ backend/src/main/java/com/blogger/backend/service/MailService.java
@@ -1,9 +1,11 @@
 package com.blogger.backend.service;
 
+import com.blogger.backend.model.User;
+
 public interface MailService {
     void sendSimpleMessage(String to, String subject, String text);
     void sendAccountActivationEmail(String to, String token);
-    void sendAccountLockedEmail(String to);
+    void sendAccountLockedEmail(User user);
     void sendPasswordResetEmail(String to, String token);
     boolean isValidEmailFormat(String email);
 }
