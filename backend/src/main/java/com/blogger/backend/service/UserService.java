5f9a1397c8aae568d795a8a88fc54c2131e69e9c
yunuskyya <0yunuskaya0@gmail.com>
2024-09-21 22:10:18 +0300

    UserService assignRole metot imzası eklendi.

--- backend/src/main/java/com/blogger/backend/service/UserService.java
+++ backend/src/main/java/com/blogger/backend/service/UserService.java
@@ -4,6 +4,7 @@ import com.blogger.backend.dto.request.RegisterUserRequest;
 import com.blogger.backend.dto.request.UserUnLockedRequset;
 import com.blogger.backend.dto.response.GetAllUserResponse;
 import com.blogger.backend.dto.response.GetUserByIdResponse;
+import com.blogger.backend.model.enums.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -13,6 +14,7 @@ public interface UserService {
      Page<GetAllUserResponse> getAllUsers(Pageable pageable);
      GetUserByIdResponse getUserById(int id);
      void unLockedUser(UserUnLockedRequset requset);
+     void assignRole(int userId, Role newRole, int currentUserId);
 
 
 }
