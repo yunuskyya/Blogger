b2307e401f4c18fe1f3382fd8285cb7f00a63eb8
yunuskyya <0yunuskaya0@gmail.com>
2024-09-21 22:07:11 +0300

    Post: güncellendi.

--- backend/src/main/java/com/blogger/backend/model/Post.java
+++ backend/src/main/java/com/blogger/backend/model/Post.java
@@ -1,8 +1,34 @@
 package com.blogger.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
 
+    @Lob
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
 }
